b802163c1501e83ca003d41be8f8b47e959689c2
Tor Arvid Lund <tor.arvid.lund@schibsted.com>
2018-03-04 22:21:04 +0100

    particle.h: Fix whitespace

--- source/particle.h
+++ source/particle.h
@@ -15,43 +15,35 @@
 //#define RAND_MAX 65536
 #define FRAND   (((float)rand()-(float)rand())/RAND_MAX)
 struct particle{
-  Vector3f pos			;	// Holds the current position
-  Vector3f dir			;	// Holds the direction 
-  float red,green,blue	;	// Holds the color of the particle
-  float life				;	// Holds how long the particle will
-  float fade				;	// Life is decreased with this until life is zero then the particle is dead
-  float size				;
-  float sizedelta			;
+  Vector3f pos; // Holds the current position
+  Vector3f dir; // Holds the direction 
+  float red,green,blue; // Holds the color of the particle
+  float life; // Holds how long the particle will
+  float fade; // Life is decreased with this until life is zero then the particle is dead
+  float size;
+  float sizedelta;
 };
 class CParticleSystem  
 {
-
-
-  private :
+  private:
     int m_wait;
     int m_nActive;
-    int m_Frames			;	// How many frame since reset
-    int m_nParticles		;	// Number of particles
-    particle* m_pParticles  ;	// Pointer to our particle-array	
-    GLuint m_texture		;	
-    Vector3f m_location		;	// The particle-systems orientation
-    const char* m_strFile	;	// The 
-  private :
+    int m_Frames; // How many frame since reset
+    int m_nParticles; // Number of particles
+    particle* m_pParticles; // Pointer to our particle-array        
+    GLuint m_texture;        
+    Vector3f m_location; // The particle-systems orientation
+    const char* m_strFile; // The file name for the bitmap
+  private:
     void update();
     void grow();
     void InitParticles();
     void GenerateTexture();
-
-
-
-
   public:
     void Reset();
-
     void Render();
     CParticleSystem(int nmbr ,Vector3f loc ,const char* bmpfile);
     virtual ~CParticleSystem();
-
 };
 
 #endif 
